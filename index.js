4418fd4798a72060d419e84182565e31c61b0300
ArthurBosch <96887759+ArthurBosch@users.noreply.github.com>
2022-08-23 03:10:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,27 +6,30 @@ const DOMElements = {
   shiftInfoMin: document.querySelector("#shiftInfoMin"),
   circleButton: document.querySelector("#circleButton"),
   sidenav: document.querySelector("#navigation"),
-  body: document.querySelector("body"),
+  main: document.querySelector("#main"),
 };
 
 //FUNCS
 const showNav = () => {
   console.log("click");
   DOMElements.sidenav.style.right = 0;
-  DOMElements.navIcon.classList.add("hidden");
+  DOMElements.navIcon.classList.add("visibility-hidden");
   DOMElements.overlay.classList.add("overlay");
 };
 
 const hideNav = () => {
   console.log("lay");
   DOMElements.sidenav.style.right = "-800px";
-  DOMElements.navIcon.classList.remove("hidden");
+  DOMElements.navIcon.classList.remove("visibility-hidden");
   DOMElements.overlay.classList.remove("overlay");
-  DOMElements.shiftInfo.style.bottom = "-270px";
 };
 
 const showShiftInfo = () => {
   DOMElements.shiftInfo.style.bottom = 0;
+};
+
+const hideShiftInfo = () => {
+  DOMElements.shiftInfo.style.bottom = "-270px";
 };
 
 //LISTENERS
@@ -34,6 +37,7 @@ const setupListeners = () => {
   DOMElements.navIcon.addEventListener("click", showNav);
   DOMElements.overlay.addEventListener("click", hideNav);
   DOMElements.circleButton.addEventListener("click", showShiftInfo);
+  DOMElements.main.addEventListener("click", hideShiftInfo);
 };
 
 setupListeners();
